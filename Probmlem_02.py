2c73f8235800a01701edeb743b126330febe0238
Vijaylachamsetty <81022005+Vijaylachamsetty@users.noreply.github.com>
2023-09-12 17:11:18 +0530

    Update Probmlem_02.py

--- Probmlem_02.py
+++ Probmlem_02.py
@@ -1,19 +1,13 @@
-# vij=dict()
-# for i in range(27):
-#     alphabet=input("enter alphabet:")
-#     vij[alphabet]=ord(alphabet)
-# print(vij)
+vij=dict()
+for i in range(27):
+    alphabet=input("enter alphabet:")
+    vij[alphabet]=ord(alphabet)
+print(vij)
 
-# def item(x):
-#     return vij[x]
-# sorted_keys = sorted(vij, key=item)
-# v = {}
-# for i in sorted_keys:
-#     v[i] = vij[i]
-# print("Sorted Dictionary:\n",v)
-
-
-# print( dict([['foo', 100]]))
-
-import os
-print(os.system('ipconfig'))
+def item(x):
+    return vij[x]
+sorted_keys = sorted(vij, key=item)
+v = {}
+for i in sorted_keys:
+    v[i] = vij[i]
+print("Sorted Dictionary:\n",v)
